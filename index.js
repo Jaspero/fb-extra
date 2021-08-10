6ff41cf3ff3d398350c3766c63d8338f0a746d55
AntonioStipic <antonio.stipic2@gmail.com>
2021-08-10 15:18:11 +0200

    chore: use commander aliases instead of custom implementation

--- index.js
+++ index.js
@@ -41,50 +41,50 @@ async function init() {
     commands.auth.description('Manage Firebase Authentication');
     commands.auth.helpOption(false);
 
-    [new Command('create-user'), new Command('cu').command('cu', {hidden: true})].map(command => {
-        return command.description('Create a new user.')
+    commands.auth.addCommand(
+        new Command('create-user')
+            .alias('cu')
+            .description('Create a new user.')
             .addArgument(new Argument('<email>', 'Email'))
             .addArgument(new Argument('<password>', 'Password'))
             .addArgument(new Argument('[custom-claims]', 'Custom Claims'))
             .action(auth.createUser)
-    }).forEach(command => {
-        commands.auth.addCommand(command);
-    });
+    );
 
-    [new Command('update-claims'), new Command('uc').command('uc', {hidden: true})].map(command => {
-        return command.description('Update users custom claims.')
+    commands.auth.addCommand(
+        new Command('update-claims')
+            .alias('uc')
+            .description('Update users custom claims.')
             .addArgument(new Argument('<id/email>', 'ID/Email'))
             .addArgument(new Argument('<custom-claims>', 'Custom Claims'))
             .action(auth.updateClaims)
-    }).forEach(command => {
-        commands.auth.addCommand(command);
-    });
+    );
 
-    [new Command('change-password'), new Command('cp').command('cp', {hidden: true})].map(command => {
-        return command.description('Changes the users password.')
+    commands.auth.addCommand(
+        new Command('change-password')
+            .alias('cp')
+            .description('Changes the users password.')
             .addArgument(new Argument('<id/email>', 'ID/Email'))
             .addArgument(new Argument('<new-password>', 'New Password'))
             .action(auth.changePassword)
-    }).forEach(command => {
-        commands.auth.addCommand(command);
-    });
+    );
 
-    [new Command('remove-user'), new Command('ru').command('ru', {hidden: true})].map(command => {
-        return command.description('Removes the user by Email or ID')
+    commands.auth.addCommand(
+        new Command('remove-user')
+            .alias('ru')
+            .description('Removes the user by Email or ID')
             .addArgument(new Argument('<id/email>', 'ID/Email'))
             .action(auth.removeUser)
-    }).forEach(command => {
-        commands.auth.addCommand(command);
-    });
+    );
 
-    [new Command('list-users'), new Command('lu').command('lu', {hidden: true})].map(command => {
-        return command.description('List app users.')
+    commands.auth.addCommand(
+        new Command('list-users')
+            .alias('lu')
+            .description('List app users.')
             .addArgument(new Argument('[page-size]', 'Page Size').default(100))
             .addArgument(new Argument('[page-number]', 'Page Number'))
             .action(auth.listUsers)
-    }).forEach(command => {
-        commands.auth.addCommand(command);
-    });
+    );
 
     /**
      * UPDATE
